41e41e0486b5dadde5284bc45bb684baf3124388
Juneid Teklu <te.juneid@gmail.com>
2024-10-16 11:38:54 -0500

    Finish implementing code that serves static HTML files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import "./styles.css";
-/*  */
+let http = require("http");
+let fs = require("node:fs");
+
+let siteServer = http
+  .createServer((request, response) => {
+    let webPage = request.url;
+    if (webPage === "/contact") {
+      response.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      fs.readFile("./Pages/contact-me.html", "utf8", function (err, data) {
+        if (err) throw err;
+        response.write(data);
+        return response.end();
+      });
+    } else if (webPage === "/about") {
+      response.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      fs.readFile("./Pages/about.html", "utf8", function (err, data) {
+        if (err) throw err;
+        response.write(data);
+        return response.end();
+      });
+    } else if (webPage === "/home") {
+      response.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+
+      fs.readFile("./Pages/index.html", "utf8", function (err, data) {
+        if (err) throw err;
+        response.write(data);
+        response.end();
+      });
+    } else {
+      response.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+      fs.readFile("./Pages/404.html", "utf-8", function (err, data) {
+        if (err) throw err;
+        response.write(data);
+        response.end();
+      });
+    }
+  })
+  .listen(3000);
